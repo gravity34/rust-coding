722746b2622ffe718f7a007c16b07dc69c50dc75
gravity34 <16541089@firat.edu.tr>
2019-10-02 09:21:35 +0300

    me

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,4 +1,68 @@
-fn main() {
+/* fn main() {
     println!("Hello, world!");
-    println!("Hello, world! its my second program");
+    another_function();
 }
+fn another_function() {
+    println!("Another function.");
+} */
+
+
+/* fn main() {
+    another_function(5, 6);
+}
+
+fn another_function(x: i32, y: i32) {
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+} */
+
+// Statements perform some action and do not return a value
+//Expressions evaluate to a resulting value
+//let keyword is a statement
+//Function definitions are also statements;
+//The block that we use to create new scopes, {}, is an expression,
+//Rust is an expression-based language
+
+/* fn main() {
+    let x = (let y = 6);        //Statements do not return values. so error will occur
+} */
+
+
+/* fn main() {
+    let x = 5;
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {}", y);
+} */
+
+
+
+
+// function returning value
+/* fn five() -> i32 {
+    5
+}
+
+fn main() {
+    let x = five();
+
+    println!("The value of x is: {}", x);
+} */
+
+
+fn main() {
+    let x = plus_one(5);
+
+    println!("The value of x is: {}", x);
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1      //adding ; will give error it will convert expression into statement which return nothing
+}
+
+
+
